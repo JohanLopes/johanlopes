d2a1eea3707600bf1f19fb52fdb5451efdb6057c
Johan LOPES <johan.lopes@elao.com>
2013-08-30 18:56:49 +0200

    Configure Capifony deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -5,45 +5,49 @@
 set :application,       "johanlopes"
 set :domain,            "uranus.creative-nao.fr"
 set :user,              "jlopes"
+set :app_path,          "app"
+set :app_web,           "web"
 set :deploy_to,         "/home/#{application}.fr"
 set :group_writable,    true
-
-#############################################################
-#   SCM
-#############################################################
- 
-set :scm,           :git
-set :scm_username,  "git"
-set :repository,    "git@#{domain}:#{application}.git"
-set :branch,        "master"
-set :deploy_via,    :remote_cache
-set :scm_verbose,   true
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
 role :db,         domain, :primary => true       # This is where Rails migrations will run
 
 #############################################################
+#   SCM
+#############################################################
+
+set :scm,           :git
+set :scm_username,  "git"
+set :repository,    "git@github.com:JohanLopes/#{application}.git"
+set :branch,        "master"
+set :deploy_via,    :remote_cache
+set :scm_verbose,   true
+
+#############################################################
 #   Config
 #############################################################
 
-set :model_manager,         "doctrine"
-set :update_vendors,        true
-set :vendors_mode,          "install"
-set :git_enable_submodules, true
+set :use_composer,          true
 set :dump_assetic_assets,   true
- 
-set :shared_files,          ["app/config/parameters.ini"]
+set :clear_controllers,     false
+
+set :shared_files,          ["app/config/parameters.yml"]
 set :shared_children,       [app_path + "/logs", web_path + "/uploads", web_path + "/media", "vendor"]
 
 set :use_sudo,              false
 set :keep_releases,         3
-set :php_bin,               '/usr/local/zend/bin/php' 
+set :php_bin,               '/usr/local/zend/bin/php'
 
 ssh_options[:forward_agent] = true
+default_run_options[:pty]   = true
 
 #############################################################
 # TASKS
 #############################################################
- 
+
 after :deploy, 'deploy:cleanup'
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
