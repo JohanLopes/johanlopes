3c0876579948fc78aeef7b2672d6028996ef7d10
Johan LOPES <johan.lopes@elao.com>
2014-08-15 11:53:59 +0200

    Update deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,11 +3,16 @@
 #############################################################
 
 set :application,       "johanlopes"
-set :domain,            "uranus"
+set :domain,            "wolverine.johanlopes.fr"
+set :user,              "jlopes"
 set :app_path,          "app"
 set :app_web,           "web"
-set :deploy_to,         "/home/#{application}.fr"
+set :deploy_to,         "/var/www/#{application}"
 set :group_writable,    true
+
+set :webserver_user,        "www-data"
+set :permission_method,     :acl
+set :use_set_permissions,   true
 
 role :web,        domain                         # Your HTTP server, Apache/etc
 role :app,        domain                         # This may be the same as your `Web` server
@@ -29,15 +34,16 @@ set :scm_verbose,   true
 #############################################################
 
 set :use_composer,          true
+set :copy_vendors, 			true
 set :dump_assetic_assets,   true
-set :clear_controllers,     false
+set :clear_controllers,     true
 
+set :writable_dirs,         ["app/cache", "app/logs", "web/media", "web/uploads"]
 set :shared_files,          ["app/config/parameters.yml"]
-set :shared_children,       [app_path + "/logs", web_path + "/uploads", web_path + "/media", "vendor"]
+set :shared_children,       [app_path + "/logs", web_path + "/uploads", web_path + "/media"]
 
 set :use_sudo,              false
-set :keep_releases,         3
-set :php_bin,               '/usr/local/zend/bin/php'
+set :keep_releases,         5
 
 ssh_options[:forward_agent] = true
 default_run_options[:pty]   = true
